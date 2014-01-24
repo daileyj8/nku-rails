69b5cdb5d06ef45488121820e608d5bc7446ef28
daileyj8 <daileyj8@nku.edu>
2014-01-24 17:57:16 +0000

    End of 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,13 @@ def update
   end
 end
 
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
 private
   def post_params
     params.require(:post).permit(:title, :text)
