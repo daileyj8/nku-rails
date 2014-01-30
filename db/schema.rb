bf01580971c2479ac2aabce1cf68268e726122be
John Dailey <daileyj8@nku.edu>
2014-01-30 17:21:27 +0000

    Student creation and list view are up and running, after several frustration fails and restarts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124180413) do
+ActiveRecord::Schema.define(version: 20140129225750) do
+
+  create_table "buellers", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +35,15 @@ ActiveRecord::Schema.define(version: 20140124180413) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
