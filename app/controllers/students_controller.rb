d08d3ab20248d83aa75d5945563a84bbafc4fc8d
John Dailey <daileyj8@nku.edu>
2014-01-31 05:21:58 +0000

    Edit/Replication problem fixed and flash messages created when adding/editing.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,18 @@
 class StudentsController < ApplicationController
 
   def new
+    @student = Student.new
   end
   
   def create
-    @student = Student.new(student_params)
-    @student.save
-    redirect_to @student
+    @student = Student.new(params[:student].permit(:name, :nickname, :email, :image))
+    
+    if @student.save
+      redirect_to students_path, notice: "Student successfully created."
+      
+    else
+      render 'new'
+    end
   end
   
   def show
@@ -15,6 +21,25 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+  end
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(params[:student].permit(:name, :nickname, :email, :image))
+      redirect_to students_path, notice: "Student successfully updated."
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
   end
   
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ActiveRecord::Base
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true  
 end
