efa04e57689e1fd3330352dac2cde5e489dd81f2
John Dailey <daileyj8@nku.edu>
2014-01-31 20:46:08 +0000

    finished logic for image, need to move the logic to a different file.

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,14 @@
 module StudentsHelper
+  
+#  def grav_url
+#    if student.image != ""
+#      return student.image
+#    elsif student.email != ""
+#      email_address= (student.email).downcase
+#      hash = Digest::MD5.hexdigest(email_address)
+#      return "http://www.gravatar.com/avatar/#{hash}"
+#    else
+#      return "http://premium.wpmudev.org/blog/wp-content/uploads/2012/04/mystery-man-small.jpg"
+#  end  
+   
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
+require 'digest/md5'
 class Student < ActiveRecord::Base
 
   validates :name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true  
+  
+#  def grav_url
+#    if student.image != ""
+#      return student.image
+#    elsif student.email != ""
+#      email_address= (student.email).downcase
+#      hash = Digest::MD5.hexdigest(email_address)
+#      return "http://www.gravatar.com/avatar/#{hash}"
+#    else
+#      return "http://premium.wpmudev.org/blog/wp-content/uploads/2012/04/mystery-man-small.jpg"
+#  end  
 end
