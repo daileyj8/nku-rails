85c59b4a6658d2c505b7a5c34c59d3762e26e2d7
John Dailey <daileyj8@nku.edu>
2014-02-07 19:46:23 +0000

    sessions controller created and log in page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    student = Student.find_by_email(params[:email])
+    if student && student.authenticate(params[:password])
+      session[:student_id] = student.id
+      redirect_to students_path, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
+end
