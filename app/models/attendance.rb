34bd3a6cf769f4ff5e7a1b8cdf55a0d554195aa0
John Dailey <daileyj8@nku.edu>
2014-02-14 06:09:50 +0000

    fixed error with no duplications not allowing new day attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,7 @@
 class Attendance < ActiveRecord::Base
   belongs_to :student
   def self.dup
-    grouped= all.group_by{|attendance| [attendance.student_id] }
+    grouped= all.group_by{|attendance| [attendance.student_id,attendance.attended_on] }
     grouped.values.each do |duplicates|
       first_one= duplicates.shift
       duplicates.each{|double| double.destroy}
