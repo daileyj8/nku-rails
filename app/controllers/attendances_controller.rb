15d6d1ece7e2fe3acac22b859aad5943ecd0b3b8
John Dailey <daileyj8@nku.edu>
2014-02-21 20:22:06 +0000

    still working on absent

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -27,9 +27,12 @@ class AttendancesController < ApplicationController
     end
   end
   def index
-    
-    
-   
+    date=Date.today
+    @in1 = Student.in_seat(1, date)
+    @in2 = Student.in_seat(2, date) 
+    @in3 = Student.in_seat(3, date) 
+    @in4 = Student.in_seat(4, date) 
+    @absentStudents= Student.absent(date) 
   end
   
   def get_current
